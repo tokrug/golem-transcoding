b906e3b611e2e2cfb4f4f01e35ab2fe8a0111c63
Edhendil <edhendil@gmail.com>
2020-11-21 13:56:20 +0100

    White link.

--- requestor-spring/src/main/js/src/Footer.jsx
+++ requestor-spring/src/main/js/src/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <footer className="transcoding-footer font-weight-light bg-primary">
                 <div className="container">
                     <div className="text-right py-3">
-                        <a href="https://github.com/Edhendil/golem-transcoding">Github</a>
+                        <a className="text-white" href="https://github.com/Edhendil/golem-transcoding">Github</a>
                     </div>
                 </div>
             </footer>
