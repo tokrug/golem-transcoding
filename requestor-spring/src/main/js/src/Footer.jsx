4213b43ba3050eea6bf5d4469a8f5867d09cda8b
Edhendil <edhendil@gmail.com>
2020-11-21 13:47:14 +0100

    Added link to Github repo.

--- requestor-spring/src/main/js/src/Footer.jsx
+++ requestor-spring/src/main/js/src/Footer.jsx
@@ -5,7 +5,8 @@ export default function Footer() {
         <React.Fragment>
             <footer className="transcoding-footer font-weight-light bg-primary">
                 <div className="container">
-                    <div className="text-right py-3">Github
+                    <div className="text-right py-3">
+                        <a href="https://github.com/Edhendil/golem-transcoding">Github</a>
                     </div>
                 </div>
             </footer>
