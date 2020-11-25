3a3524d4940a509f3a7fc5933d02e70caaea9f7b
Edhendil <edhendil@gmail.com>
2020-11-25 18:19:09 +0100

    Added webflux max file configuration.

--- requestor-spring/src/main/java/pl/krug/yagna/transcoding/spring/WebFluxConfiguration.java
+++ requestor-spring/src/main/java/pl/krug/yagna/transcoding/spring/WebFluxConfiguration.java
@@ -1,14 +1,35 @@
 package pl.krug.yagna.transcoding.spring;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.multipart.MultipartHttpMessageReader;
+import org.springframework.http.codec.multipart.SynchronossPartHttpMessageReader;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import pl.krug.yagna.transcoding.configuration.ScriptConfiguration;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebFluxConfiguration implements WebFluxConfigurer {
+
+    private final ScriptConfiguration scriptConfiguration;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
+    }
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        SynchronossPartHttpMessageReader partReader = new SynchronossPartHttpMessageReader();
+        partReader.setMaxParts(1);
+        partReader.setMaxDiskUsagePerPart(scriptConfiguration.getMaxUpload().toBytes());
+        partReader.setEnableLoggingRequestDetails(true);
+
+        MultipartHttpMessageReader multipartReader = new MultipartHttpMessageReader(partReader);
+        multipartReader.setEnableLoggingRequestDetails(true);
+
+        configurer.defaultCodecs().multipartReader(multipartReader);
     }
 }
